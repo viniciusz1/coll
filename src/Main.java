fe0505090b00367c50be3a30f84a3833aa4a4f9a
viniciusz1 <vinicius101313@gmail.com>
2022-08-25 21:51:54 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa(123,"Otavyo",12);
+        Pessoa pessoa2 = new Pessoa(321,"Tyago",17);
+        Pessoa pessoa3 = new Pessoa(231,"Dyego",32);
+        Pessoa pessoa4 = new Pessoa(132,"Gosttavu",48);
+        Pessoa pessoa5 = new Pessoa(213,"Vaitor",29);
+        Pessoa pessoa6 = new Pessoa(312,"Quenso",37);
+        Pessoa pessoa7 = new Pessoa(312,"Quenso2",37);
+
+        Set<Pessoa> pessoaSet = new TreeSet<>();
+        Collections.addAll(pessoaSet,pessoa1,pessoa2,pessoa3,pessoa4,pessoa5,pessoa6,pessoa7, pessoa5);
+        pessoaSet.add(pessoa1);
+        pessoaSet.add(pessoa5);
+        System.out.println(pessoaSet);
+    }
+}
+
---
+++ src/Pessoa.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Pessoa implements Comparable{
+
+    int cpf;
+    String nome;
+    int idade;
+
+
+    @Override
+    public boolean equals(Object o) {
+        Pessoa outra = (Pessoa) o;
+        return this.cpf == (outra.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "CPF=" + cpf +
+                ", Nome=" + nome +
+                ", Idade=" + idade +"\n";
+    }
+
+    public Pessoa(int cpf, String nome, int idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return this.idade - o.idade;
+    }
+}
